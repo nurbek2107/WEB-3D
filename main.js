ecfb8b0174c438e642a19786291f357e0cf420b8
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-04-14 15:28:09 +0500

    Version 0.1

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/BBvnWpitLW1zAD48/scene.splinecode');
